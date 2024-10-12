3a7ac4b9570115164647a1d4e4bad85d04729550
Priyanka yadav <priyanyadav52004@gmail.com>
2024-10-12 18:03:39 +0530

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -16,24 +18,33 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     }
 
     // Database call
-    const user = await userModel.findOne({ email: email });
+    try {
+      const user = await userModel.findOne({ email: email });
 
-    if (user) {
-      const error = createHttpError(
-        400,
-        "User already exists with this email."
-      );
-      return next(error);
+      if (user) {
+        const error = createHttpError(
+          400,
+          "User already exists with this email."
+        );
+        return next(error);
+      }
+    } catch (err) {
+      return next(createHttpError(500, "Error while getting user"));
     }
 
     // password -> hashed
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await userModel.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
+    let newUser: User;
+    try {
+      newUser = await userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+    } catch (error) {
+      return next(createHttpError(500, "Error while creating user."));
+    }
 
     // Token generation JWT
     const token = sign({ sub: newUser._id }, config.JwtSecret as string, {
@@ -43,8 +54,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     // Simulating user creation
     res.status(201).json({ accessToken: token });
   } catch (error) {
-    // Pass any unexpected errors to the global error handler
-    next(error);
+    return next(createHttpError(500, "Error while signing the jwt token"));
   }
 };
 
